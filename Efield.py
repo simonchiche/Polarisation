ff82ad4fc6e0aede212ed52d1e71a921aebe063c
simonchiche <simon.chiche@obspm.fr>
2020-03-15 18:39:06 +0100

    Electric field p2p plot at ground level

--- Efield.py
+++ Efield.py
@@ -9,19 +9,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-x,y,z, Ex, Ey = np.loadtxt('gamma.txt', unpack = True) #x = x_antenna, y = y_antenna, z = Etot
+x,y,Etot, Ex, Ey = np.loadtxt('gamma.txt', unpack = True) #x = x_antenna, y = y_antenna
 fig, ax0 = plt.subplots()
 
 
-im0 = plt.scatter(x,y,s=1,c=z, cmap='bwr')
+im0 = plt.scatter(x,y,s=1,c=Etot, cmap='bwr') 
 
 ax0.set_title("Electricf_p2p")
 plt.xlabel("x [m]")
 plt.ylabel("y [m]")
 cbar = fig.colorbar(im0,ax=ax0)
+cbar.set_label(r"$ E\ [\mu V/m]$")
 plt.savefig('Efield.png', dpi = 2000)
 
-    
-    
 
-
+    
